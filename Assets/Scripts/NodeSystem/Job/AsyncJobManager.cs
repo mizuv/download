abb72c4c482a824b88a95d1b651ecfadf49b687a
mizuv <mizuvt@gmail.com>
2024-07-26 17:12:49 +0900

    feat: AsyncJobManager에 runCancel observable 추가

--- Assets/Scripts/NodeSystem/Job/AsyncJobManager.cs
+++ Assets/Scripts/NodeSystem/Job/AsyncJobManager.cs
@@ -14,15 +14,17 @@ namespace Download.NodeSystem {
     public class AsyncJobManager {
         private static readonly float JOB_UPDATE_SECOND = 0.0625f;
 
-        private ReactiveProperty<bool> autoRun = new ReactiveProperty<bool>(false);
+        private bool autoRun = false;
         private readonly ReactiveProperty<float?> _runtime = new(null);
         private readonly Subject<Unit> _runCompleteSubject = new Subject<Unit>();
         private readonly Subject<Unit> _runTerminateSubject = new Subject<Unit>();
+        private readonly Subject<Unit> _runCancelSubject = new Subject<Unit>();
         private readonly Subject<Unit> _runCancelTriggerSubject = new Subject<Unit>();
 
         public IReadOnlyReactiveProperty<float?> Runtime => _runtime;
         public IObservable<Unit> RunComplete => _runCompleteSubject.AsObservable();
         public IObservable<Unit> RunTerminate => _runTerminateSubject.AsObservable();
+        public IObservable<Unit> RunCancel => _runCancelSubject.AsObservable();
 
         protected readonly IEnumerable<CompositeDisposable> _disposablesList;
         public readonly AsyncJobOption AsyncJobOption;
@@ -30,6 +32,13 @@ namespace Download.NodeSystem {
         public AsyncJobManager(IEnumerable<CompositeDisposable> disposablesList, AsyncJobOption asyncJobOption) {
             _disposablesList = disposablesList;
             AsyncJobOption = asyncJobOption;
+
+            var subscription = RunComplete.Subscribe(_ => {
+                if (autoRun) {
+                    Run();
+                }
+            });
+            _disposablesList.ForEach(disposables => disposables.Add(subscription));
         }
 
         public void StopRun() {
@@ -46,22 +55,26 @@ namespace Download.NodeSystem {
 
             })
                 .TakeUntil(_runCancelTriggerSubject)
-                .DoOnTerminate(() => { _runtime.Value = null; _runTerminateSubject.OnNext(Unit.Default); })
+                .DoOnTerminate(() => {
+                    var prevRuntimeValue = _runtime.Value;
+                    _runtime.Value = null;
+                    if (prevRuntimeValue < AsyncJobOption.RunDuration)
+                        _runCancelSubject.OnNext(Unit.Default);
+                    if (prevRuntimeValue >= AsyncJobOption.RunDuration)
+                        _runCompleteSubject.OnNext(Unit.Default);
+                    _runTerminateSubject.OnNext(Unit.Default);
+                })
                 .Subscribe(timeElapsed => {
                     _runtime.Value = timeElapsed;
                     if (_runtime.Value >= AsyncJobOption.RunDuration) {
                         StopRun();
-                        _runCompleteSubject.OnNext(Unit.Default);
-                        if (autoRun.Value) {
-                            Run();
-                        }
                     }
                 });
             _disposablesList.ForEach(disposables => disposables.Add(subscription));
         }
 
         public void SetAuto(bool enable) {
-            autoRun.Value = enable;
+            autoRun = enable;
         }
     }
 }
