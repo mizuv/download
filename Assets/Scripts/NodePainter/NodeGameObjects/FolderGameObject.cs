fdb2c7ff13c8e9012eaa5bef4d042247a6b14d8a
mizuv <mizuvt@gmail.com>
2024-07-28 15:56:08 +0900

    feat: 폴더 열리고 닫혔을때 sprite 다르게 하기

--- Assets/Scripts/NodePainter/NodeGameObjects/FolderGameObject.cs
+++ Assets/Scripts/NodePainter/NodeGameObjects/FolderGameObject.cs
@@ -7,18 +7,21 @@ using UnityEngine;
 namespace Download {
     public class FolderGameObject : NodeGameObject {
         public Transform ChildrenContainer;
+        public SpriteRenderer SpriteRenderer;
+        public Sprite OpenSprite;
+        public Sprite ClosedSprite;
         [NonSerialized]
         public ReactiveProperty<bool> IsOpen = new ReactiveProperty<bool>(true);
 
         public override void Start() {
             base.Start();
             IsOpen.Subscribe(isOpen => {
-                Debug.Log(IsOpen.Value);
                 ChildrenContainer.gameObject.SetActive(isOpen);
+                if (OpenSprite != null && ClosedSprite != null)
+                    SpriteRenderer.sprite = isOpen ? OpenSprite : ClosedSprite;
             }).AddTo(this);
 
             DoubleClick.Subscribe(_ => {
-                Debug.Log("Doubleclick detected");
                 IsOpen.Value = !IsOpen.Value;
             }).AddTo(this);
         }
