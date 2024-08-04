db25bfb50403772335b49bc82ab7adfee4cd12f1
mizuv <mizuvt@gmail.com>
2024-08-05 02:10:49 +0900

    feat: Breach를 실행가능하게

--- Assets/Scripts/NodeSystem/Node/Branch.cs
+++ Assets/Scripts/NodeSystem/Node/Branch.cs
@@ -1,6 +1,25 @@
+using System;
+using UniRx;
+
 namespace Download.NodeSystem {
-    public class Branch : Node {
-        public Branch(Folder parent, string name, NodeCreateOptions? nodeCreateOptions = null) : base(parent, name, nodeCreateOptions) { }
+    public class Branch : Node, IRunnable {
+        private RunOption _runOption = new RunOption(4000);
+        public bool RunByPanel => true;
+        public IReadOnlyReactiveProperty<float?> Runtime => RunManager.Runtime;
+        public IObservable<Unit> RunComplete => RunManager.RunComplete;
+        public IReadOnlyReactiveProperty<bool> IsRunActive => RunManager.IsActive;
+        public override RunOption RunJobOption => _runOption;
+
+        public Branch(Folder parent, string name, NodeCreateOptions? nodeCreateOptions = null) : base(parent, name, nodeCreateOptions) {
+
+            RunComplete
+                .Subscribe(_ => {
+                    if (Parent == null) return;
+                    new Stick(this.Parent.ChildRunResultTarget, "막대기", new NodeCreateOptions { Index = GetIndex() + 1 });
+                    this.Delete();
+                })
+                .AddTo(_disposables);
+        }
 
         public override float Volume => 0.8f;
         public override string GetPrintString(string indent) {
@@ -10,5 +29,18 @@ namespace Download.NodeSystem {
         public override IStaticNode GetStaticNode() {
             return StaticNode;
         }
+
+        public void StartRun() {
+            RunManager.StartRun();
+        }
+
+        public void StopRun() {
+            RunManager.StopRun();
+        }
+
+        public void SetAutoRun(bool enable) {
+            RunManager.SetAuto(enable);
+        }
+
     }
 }
